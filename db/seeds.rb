5936a86121446fec761e5d821780b7032c97e750
Ubuntu <ubuntu@ip-172-31-31-163.ap-northeast-1.compute.internal>
2026-03-20 05:08:40 +0000

    テスト用のseedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+puts "CheerTASKのデータ作成を開始します..."
+
+# 1人目：山田さん
+User.find_or_create_by!(email: "yamada@example.com") do |user|
+  user.name = "山田太郎"
+  user.password = "password"
+end
+
+# 2人目：田中さん
+User.find_or_create_by!(email: "tanaka@example.com") do |user|
+  user.name = "田中次郎"
+  user.password = "password"
+end
+
+puts "データ作成が完了しました！"
