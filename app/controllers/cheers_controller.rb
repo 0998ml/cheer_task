be33c2d78c3d34400316a6e8a803813abfdd5be2
Ubuntu <ubuntu@ip-172-31-31-163.ap-northeast-1.compute.internal>
2026-03-29 09:08:18 +0000

    CheersControllerをDRYに共通化

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,7 +1,9 @@
 class CheersController < ApplicationController
+  # 1. 共通の「タスク探し」を、アクションが動く前に実行
+  before_action :set_task, only: [:create, :destroy]
 
   def create
-    @task = Task.find(params[:task_id])
+    
     # current_user（自分）のcheersとして新しいデータを作る
     cheer = current_user.cheers.new(task_id: @task.id)
     cheer.save
@@ -10,11 +12,17 @@ class CheersController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:task_id])
+    
     # 自分のcheersの中から、このタスクにつけたCheerを探して削除する
     cheer = current_user.cheers.find_by(task_id: @task.id)
     cheer.destroy
 
     redirect_back(fallback_location: tasks_path)
+  end
+
+  private
+
+  def set_task 
+    @task = Task.find(params[:task_id])
   end
 end
